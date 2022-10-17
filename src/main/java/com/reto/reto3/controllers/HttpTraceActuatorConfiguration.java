fa21d5e1ba7f4ec4bf54808c529282ac196bbd7a
Jeanet Salamanca <ana.salamanca.mt@usa.edu.co>
2022-10-17 14:36:25 -0500

    update trace

--- src/main/java/com/reto/reto3/controllers/HttpTraceActuatorConfiguration.java
+++ src/main/java/com/reto/reto3/controllers/HttpTraceActuatorConfiguration.java
@@ -4,13 +4,10 @@ import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
 import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class HttpTraceActuatorConfiguration {
 
-    @Bean
-    public HttpTraceRepository httpTraceRepository() {
-        return new InMemoryHttpTraceRepository();
-    }
 
 }
